e154c2322f970d0fd3f54b8dd91729c07856ca7a
neo-30003 <dr.arrum@gmail.com>
2024-07-14 20:16:17 +0300

    Выполнил 20 урок

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import timer from "./modules/timer";
+import menu from "./modules/menu";
+import modal from "./modules/modal";
+import scroll from "./modules/scroll";
+
+timer("12 august 2024");
+menu();
+modal();
+scroll();
