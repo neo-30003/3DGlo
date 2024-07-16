bd2362aac32d8d2c74fc080f62998876294674c9
neo-30003 <dr.arrum@gmail.com>
2024-07-16 19:26:59 +0300

    Выполнил 21-й урок

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import timer from "./modules/timer";
+import menu from "./modules/menu";
+import modal from "./modules/modal";
+import scroll from "./modules/scroll";
+import calculator from "./modules/calculator";
+import form from "./modules/form";
+
+timer("12 august 2024");
+menu();
+modal();
+scroll();
+calculator();
+form();
