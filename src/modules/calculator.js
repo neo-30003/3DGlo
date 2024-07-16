75422e600dc6d0133e0804a0a7f099458b35160f
neo-30003 <dr.arrum@gmail.com>
2024-07-16 19:27:20 +0300

    21-урок

---
+++ src/modules/calculator.js
@@ -0,0 +1,10 @@
+const calculator = () => {
+  const calcInputs = document.querySelectorAll("input.calc-item");
+  calcInputs.forEach((item) => {
+    item.addEventListener("input", (e) => {
+      e.target.value = e.target.value.replace(/\D+/g, "");
+    });
+  });
+};
+
+export default calculator;
---
+++ src/modules/modal.js
@@ -0,0 +1,52 @@
+const modal = () => {
+  const modal = document.querySelector(".popup");
+  const buttons = document.querySelectorAll(".popup-btn");
+  const closeBtn = modal.querySelector(".popup-close");
+  const media = window.matchMedia("(min-width: 768px)");
+
+  let count = 0;
+  let idInterval;
+
+  if (media.matches) {
+    const animatePopup = () => {
+      count = count + 5;
+      idInterval = requestAnimationFrame(animatePopup);
+      modal.style.display = "block";
+
+      if (count < 100) {
+        modal.style.opacity = count + "%";
+      } else {
+        cancelAnimationFrame(idInterval);
+      }
+    };
+
+    const unpopup = () => {
+      count = count - 5;
+      idInterval = requestAnimationFrame(unpopup);
+
+      if (count > 0) {
+        modal.style.opacity = count + "%";
+      } else {
+        cancelAnimationFrame(idInterval);
+        modal.style.display = "none";
+      }
+    };
+
+    buttons.forEach((btn) => {
+      btn.addEventListener("click", animatePopup);
+    });
+    closeBtn.addEventListener("click", unpopup);
+  } else {
+    buttons.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        modal.style.display = "block";
+      });
+    });
+
+    closeBtn.addEventListener("click", () => {
+      modal.style.display = "none";
+    });
+  }
+};
+
+export default modal;
