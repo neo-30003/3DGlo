389c06ae52253772620f00f3a91871cd8579d4c1
neo-30003 <dr.arrum@gmail.com>
2024-07-18 23:14:26 +0300

    Выполнил 23-й урок

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import timer from "./modules/timer";
+import menu from "./modules/menu";
+import modal from "./modules/modal";
+import scroll from "./modules/scroll";
+import calculator from "./modules/calculator";
+import form from "./modules/form";
+import tabs from "./modules/tabs";
+import slider from "./modules/slider";
+
+timer("12 august 2024");
+menu();
+modal();
+scroll();
+calculator();
+form();
+tabs();
+slider(".portfolio-content", ".portfolio-item", ".dot");
