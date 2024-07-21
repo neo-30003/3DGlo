758f71a95eef34f136c73e48924d8cd24c3c3b05
neo-30003 <dr.arrum@gmail.com>
2024-07-21 12:18:11 +0300

    lesson27

---
+++ src/modules/menu.js
@@ -0,0 +1,23 @@
+const menu = () => {
+  const menu = document.querySelector("menu");
+
+  const handleMenu = () => {
+    menu.classList.toggle("active-menu");
+  };
+
+  document.addEventListener("click", (e) => {
+    if (e.target.closest(".close-btn")) {
+      handleMenu();
+    }
+    if (e.target.closest(".active-menu") && e.target.hasAttribute("href")) {
+      handleMenu();
+    }
+    if (!e.target.closest("menu") && menu.classList.contains("active-menu")) {
+      handleMenu();
+    } else if (e.target.closest(".menu")) {
+      handleMenu();
+    }
+  });
+};
+
+export default menu;
---
+++ src/modules/modal.js
@@ -0,0 +1,74 @@
+import { animate } from "./helpers";
+
+const modal = () => {
+  const modal = document.querySelector(".popup");
+  const buttons = document.querySelectorAll(".popup-btn");
+  const closeBtn = modal.querySelector(".popup-close");
+  const media = window.matchMedia("(min-width: 768px)");
+
+  let count = 0;
+
+  if (media.matches) {
+    const animatePopup = () => {
+      animate({
+        duration: 1000,
+        timing(timeFraction) {
+          return timeFraction;
+        },
+        draw() {
+          modal.style.display = "block";
+          if (count < 100) {
+            modal.style.opacity = count + "%";
+            count += 5;
+          }
+        },
+      });
+    };
+
+    const unpopup = () => {
+      animate({
+        duration: 1000,
+        timing(timeFraction) {
+          return timeFraction;
+        },
+        draw() {
+          if (count > 0) {
+            modal.style.opacity = count + "%";
+            count -= 5;
+          } else {
+            modal.style.display = "none";
+          }
+        },
+      });
+    };
+
+    buttons.forEach((btn) => {
+      btn.addEventListener("click", animatePopup);
+    });
+    modal.addEventListener("click", (e) => {
+      if (
+        !e.target.closest(".popup-content") ||
+        e.target.classList.contains(".popup-close")
+      ) {
+        unpopup();
+      }
+    });
+  } else {
+    buttons.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        modal.style.display = "block";
+      });
+    });
+
+    modal.addEventListener("click", (e) => {
+      if (
+        !e.target.closest(".popup-content") ||
+        e.target.classList.contains(".popup-close")
+      ) {
+        modal.style.display = "none";
+      }
+    });
+  }
+};
+
+export default modal;
